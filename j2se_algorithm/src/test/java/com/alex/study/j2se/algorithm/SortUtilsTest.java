21a3f48d45369ebcdcc26880a50ae9ec8e1ab2b4
Alex <dischannel@msn.cn>
2021-01-25 21:48:36 +0800

    add git ignore file

--- j2se_algorithm/src/test/java/com/alex/study/j2se/algorithm/SortUtilsTest.java
+++ j2se_algorithm/src/test/java/com/alex/study/j2se/algorithm/SortUtilsTest.java
@@ -7,10 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 class SortUtilsTest {
 
     @Test
-    public void should_return_right_result_when_call_inserSort_given_int_list() {
+    public void should_return_right_result_when_call_insertSort_given_int_list() {
         int expectResult[] = {1, 2, 3};
-        int originalListp[] = {2, 3, 1};
-        int actualList[] = SortUtils.insertSort(originalListp);
+        int originalList[] = {2, 3, 1};
+        int actualList[] = SortUtils.insertSort(originalList);
         assertArrayEquals(expectResult, actualList);
     }
 
